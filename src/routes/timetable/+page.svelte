f3bc8b55c420f7790c2df382b076546e81db9746
shojicode <shoji.tamaki@gmail.com>
2024-05-14 23:24:45 +0900

    二日目を打ち込み（CSSはまだ）

--- src/routes/timetable/+page.svelte
+++ src/routes/timetable/+page.svelte
@@ -3,9 +3,12 @@
 
     type Date = 
         |"First"
-        |"second_am"
-        |"second_pm";
+        |"Second";
+
+    let date: Date = 'First'
 </script>
+
+
 <main>
     <Paper title="タイムテーブル">
         <select>
@@ -15,7 +18,7 @@
         </select>
         <div class="tt">
             <div class="date">
-                <h2></h2>
+                <h2>{#if date == 'First'} 5/17{/if}</h2>
             </div>
             <div class="time"></div>
             <div class="nine">9:00</div>
@@ -25,31 +28,62 @@
             <div class="thirteen">13:00</div>
             <div class="fourteen">14:00</div>
             <div class="fifteen">15:00</div>
+
             <div class="arena">
                 <h3>アリーナ</h3>
             </div>
-            <div class="chorus">コーラス部<br>9:10-10:10</div>
-            <div class="eleventh-drama">11回生演劇<br>10:30-11:20</div>
-            <div class="drama">演劇部<br>11:40-12:20</div>
-            <div class="brass">吹奏楽部<br>14:00-14:50</div>
+            {#if date == 'First'}
+                <div class="chorus1">コーラス部<br>9:10-10:10</div>
+                <div class="eleventh-drama1">11回生演劇<br>10:30-11:20</div>
+                <div class="drama1">演劇部<br>11:40-12:20</div>
+                <div class="brass1">吹奏楽部<br>14:00-14:50</div>
+            {/if}
+
+            {#if date == 'Second'}
+                <div class="brass2">吹奏楽部<br>9:10-10:00</div>
+                <div class="eleventh-drama2">11回生演劇<br>10;40-11:30</div>
+                <div class="drama2">演劇部<br>12:20-13:10</div>
+                <div class="chorus2">コーラス部<br>13:40-14:30</div>
+            {/if}
+
             <div class="stage">
                 <h3>ステージ</h3>
             </div>
-            <div class="patata">パタータ<br>9:10-9:40</div>
-            <div class="togemanzai">とげ漫才<br>10:00-10:10</div>
-            <div class="isee">I see<br>10:40-11:00</div>
-            <div class="raccoon-friendship">アライグマの友情<br>11:10-11:30</div>
-            <div class="p-and-a">P and A<br>11:40-12:00</div>
-            <div class="asricotta">AsRicotta<br>12:10-13:50</div>
-            <div class="bingo">ビンゴ大会<br>14:10-14:50</div>
+            {#if date == 'First'}
+                <div class="patata1">パタータ<br>9:10-9:40</div>
+                <div class="togemanzai1">とげ漫才<br>10:00-10:10</div>
+                <div class="isee1">I see<br>10:40-11:00</div>
+                <div class="raccoon-friendship1">アライグマの友情<br>11:10-11:30</div>
+                <div class="p-and-a1">P and A<br>11:40-12:00</div>
+                <div class="asricotta1">AsRicotta<br>12:10-13:50</div>
+                <div class="bingo1">ビンゴ大会<br>14:10-14:50</div>
+            {/if}
+
+            {#if date == 'Second'}
+                <div class="asricotta2">AsRicotta<br>9:10-11:20</div>
+                <div class="togemanzai2">とげ漫才<br>11:30-11:40</div>
+                <div class="moritatatsuya2">モリタタツヤ<br>12:30-13:00</div>
+                <div class="unagipai2">ウナギパイ（仮）<br>13:10-13:40</div>
+                <div class="fragment2">Fragment<br>13:50-14:20</div>
+                <div class="root-and-5th2">Root & 5th<br>14:30-15:30</div>
+            {/if}
+
             <div class="musicroom">
                 <h3>音楽室</h3>
             </div>
-            <div class="moritatatsuya">モリタタツヤ<br>9:30-10:00</div>
-            <div class="uranus">七番目の惑星<br>10:10-10:40</div>
-            <div class="fragment">Fragment<br>10:50-11:20</div>
-            <div class="root-and-5th">Root & 5th<br>11:30-12:00</div>
-            <div class="unagipai-kari">ウナギパイ（仮）<br>12:10-12:40</div>
+            {#if date == 'First'}
+                <div class="moritatatsuya1">モリタタツヤ<br>9:30-10:00</div>
+                <div class="uranus1">七番目の惑星<br>10:10-10:40</div>
+                <div class="fragment1">Fragment<br>10:50-11:20</div>
+                <div class="root-and-5th1">Root & 5th<br>11:30-12:00</div>
+                <div class="unagipai-kari1">ウナギパイ（仮）<br>12:10-12:40</div>
+            {/if}
+
+            {#if date == 'Second'}
+                <div class="p-and-a2">P and A<br>10:40-11:00</div>
+                <div class="uranus2">七番目の惑星<br>11:10-11:40</div>
+                <div class="patata2">パタータ<br>12:20-12:50</div>
+            {/if}
         </div>
 
 
@@ -83,24 +117,24 @@
     .fourteen { grid-area: 32 / 2 / 33 / 3; }
     .fifteen { grid-area: 38 / 2 / 39 / 3; }
     .arena { grid-area: 1 / 3 / 2 / 4; }
-    .chorus { grid-area: 3 / 3 / 9 / 4; }
-    .eleventh-drama { grid-area: 11 / 3 / 16 / 4; }
-    .drama { grid-area: 18 / 3 / 22 / 4; }
-    .brass { grid-area: 32 / 3 / 37 / 4; }
+    .chorus1 { grid-area: 3 / 3 / 9 / 4; }
+    .eleventh-drama1 { grid-area: 11 / 3 / 16 / 4; }
+    .drama1 { grid-area: 18 / 3 / 22 / 4; }
+    .brass1 { grid-area: 32 / 3 / 37 / 4; }
     .stage { grid-area: 1 / 4 / 2 / 5; }
-    .patata { grid-area: 3 / 4 / 6 / 5; }
-    .togemanzai { grid-area: 8 / 4 / 9 / 5; }
-    .isee { grid-area: 12 / 4 / 14 / 5; }
-    .raccoon-friendship { grid-area: 15 / 4 / 17 / 5; }
-    .p-and-a { grid-area: 18 / 4 / 20 / 5; }
-    .asricotta { grid-area: 21 / 4 / 31 / 5; }
-    .bingo { grid-area: 33 / 4 / 38 / 5; }
+    .patata1 { grid-area: 3 / 4 / 6 / 5; }
+    .togemanzai1 { grid-area: 8 / 4 / 9 / 5; }
+    .isee1 { grid-area: 12 / 4 / 14 / 5; }
+    .raccoon-friendship1 { grid-area: 15 / 4 / 17 / 5; }
+    .p-and-a1 { grid-area: 18 / 4 / 20 / 5; }
+    .asricotta1 { grid-area: 21 / 4 / 31 / 5; }
+    .bingo1 { grid-area: 33 / 4 / 38 / 5; }
     .musicroom { grid-area: 1 / 5 / 2 / 6; }
-    .moritatatsuya { grid-area: 5 / 5 / 8 / 6; }
-    .uranus { grid-area: 9 / 5 / 12 / 6; }
-    .fragment { grid-area: 13 / 5 / 16 / 6; }
-    .root-and-5th { grid-area: 17 / 5 / 20 / 6; }
-    .unagipai-kari { grid-area: 21 / 5 / 24 / 6; }
+    .moritatatsuya1 { grid-area: 5 / 5 / 8 / 6; }
+    .uranus1 { grid-area: 9 / 5 / 12 / 6; }
+    .fragment1 { grid-area: 13 / 5 / 16 / 6; }
+    .root-and-5th1 { grid-area: 17 / 5 / 20 / 6; }
+    .unagipai-kari1 { grid-area: 21 / 5 / 24 / 6; }
 
     @media(max-width: 600px) {
         .tt {
